0a83a1c3daf69b929dcfae760f10f5a97840de5a
Моханнад Ваэль Саад Хамад Али Саид <msaid@edu.hse.ru>
2025-12-07 17:04:59 +0300

    rightFULLSec

--- src/component/Hero1.jsx
+++ src/component/Hero1.jsx
@@ -26,7 +26,7 @@ export default function Hero1() {
           
             <section className="bg-white w-[609px] h-[513px] mt-[82px]">
               <div className="bg-white  w-[609px] h-full">
-                <h1 className="text-[60px] font-bold  w-full h-[140px] leading-tight text-black">
+                <h1 className="text-[60px] font-bold  w-full h-[140px] leading-tight text-black slide-left opacity-0" style={{animationDelay: "0s"}}>
                   The Power of Healthy Food{" "}
                   <span
                     style={{ color: colors[index] }}
@@ -38,14 +38,14 @@ export default function Hero1() {
                   </span>
                 </h1>
 
-                <p className="w-[508px] mt-4 h-[105px] text-[20px] text-black">
+                <p className="w-[508px] mt-4 h-[105px] text-[20px] text-black slide-left opacity-0" style={{animationDelay: ".2s"}}>
                   A restaurant is an establishment that prepares and serves food
                   and drinks to customers, typically for consumption on the
                   premises, but also offering take-out or delivery.
                 </p>
 
-                <div className="flex w-[280px] mt-10 justify-between">
-                  <button className="w-[145px] h-[46px] bg-[#065B5E] flex items-center justify-center text-[#Ffff] rounded-full text-[22px] shadow-[#b3becc]">
+                <div className="flex w-[280px] mt-10 justify-between slide-left opacity-0" style={{animationDelay: ".4s"}} >
+                  <button className="w-[145px] h-[46px] bg-[#065B5E] flex items-center justify-center text-[#Ffff] rounded-full text-[22px] shadow-[#b3becc] ">
                     Order Now
                   </button>
                   <button className="w-[109px] h-[46px] bg-[#ffff] flex items-center justify-center text-[#FF4033] rounded-full text-[22px] border-[#FF4033] border-2 shadow-[#b3becc]">
